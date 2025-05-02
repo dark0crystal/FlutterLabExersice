b5957984020bfee51f4740cbd36f7bd3a5fba6e8
ALMardasDEV <albusaidi9094@gmail.com>
2025-05-02 23:43:45 +0400

    generate fake date

---
+++ lib/shared/EmailsData.dart
@@ -0,0 +1,76 @@
+
+import 'package:lab_exersice/models/EmailModel.dart';
+
+List<EmailMessage> emailList = [
+  EmailMessage(
+    senderName: "Abdullah Khamis",
+    senderEmail: "abdullah@gmail.com",
+    subject: "Welcome",
+    sentDate: "1/4/2025",
+    message: "Hi there,\n\nThis is a test for email server.",
+  ),
+  EmailMessage(
+    senderName: "Ali Salem",
+    senderEmail: "Ali@squ.edu.om",
+    subject: "Add course",
+    sentDate: "1/1/2025",
+    message: "Dear student,\n\nWe have enrolled you in COMP4206.",
+  ),
+  EmailMessage(
+    senderName: "Huda Mohammed",
+    senderEmail: "Huda@utas.edu.om",
+    subject: "Programming contest",
+    sentDate: "10/4/2025",
+    message: "Congratulations for winning the contest and best regards.",
+  ),
+  // Add 7 more emails
+  EmailMessage(
+    senderName: "Mona Saleh",
+    senderEmail: "mona@moe.edu.om",
+    subject: "Reminder",
+    sentDate: "2/4/2025",
+    message: "Don't forget the meeting tomorrow.",
+  ),
+  EmailMessage(
+    senderName: "Khalid Said",
+    senderEmail: "khalid@outlook.com",
+    subject: "Thank you",
+    sentDate: "5/4/2025",
+    message: "Thanks for your support during the event.",
+  ),
+  EmailMessage(
+    senderName: "Fatma Nasser",
+    senderEmail: "fatma@yahoo.com",
+    subject: "Invoice",
+    sentDate: "3/4/2025",
+    message: "Please find the attached invoice for your records.",
+  ),
+  EmailMessage(
+    senderName: "Yusuf Ahmed",
+    senderEmail: "yusuf@company.com",
+    subject: "Job offer",
+    sentDate: "6/4/2025",
+    message: "We are pleased to offer you the position.",
+  ),
+  EmailMessage(
+    senderName: "Reem Salim",
+    senderEmail: "reem@gmail.com",
+    subject: "Invitation",
+    sentDate: "7/4/2025",
+    message: "You are invited to attend our seminar.",
+  ),
+  EmailMessage(
+    senderName: "Salim Zayed",
+    senderEmail: "salim@domain.com",
+    subject: "Update",
+    sentDate: "8/4/2025",
+    message: "The system will be down for maintenance.",
+  ),
+  EmailMessage(
+    senderName: "Noura Talib",
+    senderEmail: "noura@service.org",
+    subject: "Newsletter",
+    sentDate: "9/4/2025",
+    message: "Here is your monthly newsletter.",
+  ),
+];
